1a221b706f5ef35a1e14676d20e6f42c4afa3ffc
wkwqiyu <1216737896@qq.com>
2025-01-08 23:08:20 +0800

    hot100-twosum java提交

---
+++ leetcode100Hot/1-两数之和/java/Solution.java
@@ -0,0 +1,17 @@
+package main;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> m = new HashMap<>();
+        for(int i=0;i< nums.length;i++){
+            if(m.containsKey(target-nums[i])){
+                return new int[]{m.get(target-nums[i]),i};
+            }
+            m.put(nums[i],i);
+        }
+        return null;
+    }
+}
